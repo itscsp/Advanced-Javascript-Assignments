dc1a92410f35c118f5b0ba9144bc573edda9b444
itscsp <chethanspoojary1@gmail.com>
2026-02-02 10:07:27 +0530

    feat: implement delay and fetchWithTimeout functions with promise handling

--- callbacks/easy/fetchWithTimeout.js
+++ callbacks/easy/fetchWithTimeout.js
@@ -5,8 +5,32 @@
 // If the request completes within the specified time, the Promise resolves with the fetched data. 
 // If the operation exceeds the time limit, the Promise rejects with the message "Request Timed Out".
 
-function fetchWithTimeout(url, ms) {}
+function fetchWithTimeout(url, ms) {
+    return new Promise((resolve, reject) => {
+        let timeoutId = setTimeout(() => {
+            reject("Request Timed Out");
+        }, ms);
+        
+        fetch(url)
+            .then(response => {
+                clearTimeout(timeoutId);
+                resolve(response);
+            })
+            .catch(error => {
+                clearTimeout(timeoutId);
+                reject(error);
+            });
+    });
+}
 
-function fetchWithTimeoutClean(url, ms) {}
+function fetchWithTimeoutClean(url, ms) {
+    const timeoutPromise = new Promise((_, reject) => {
+        setTimeout(() => {
+            reject("Request Timed Out");
+        }, ms);
+    });
+    
+    return Promise.race([fetch(url), timeoutPromise]);
+}
 
 module.exports = { fetchWithTimeout, fetchWithTimeoutClean };
